0722eae6618c5497fadf367dc276fab4f7c38935
Clown <1104480806@qq.com>
2020-08-08 22:08:33 +0800

    完成 svg

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,16 +2,28 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
+
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
-
+  font-size: 12px;
   > ul {
      display:flex;
     > li {
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
     }
   }
 `;
@@ -20,12 +32,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg fill="red" className="icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg fill="red" className="icon">
+            <use xlinkHref="#label"/>
+          </svg>
           <Link to="/labels">标签</Link>
         </li>
         <li>
+          <svg fill="red" className="icon">
+            <use xlinkHref="#statistics"/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
