e123ee3112b6a99047a0b3a1b88984d7df9abc70
Clown <1104480806@qq.com>
2020-08-08 21:24:32 +0800

    应用 react-router 和 Nav 样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
-import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display:flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+ flex-grow: 1;
+ overflow: auto;
+`;
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/labels">
+              <Labels/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+          </Switch>
+        </Main>
+      <Nav/>
+      </Wrapper>
+    </Router>
   );
+}
+
+
+function Statistics() {
+  return <h2>Home</h2>;
+}
+
+function Money() {
+  return <h2>About</h2>;
+}
+
+function Labels() {
+  return <h2>Users</h2>;
+}
+
+function NoMatch() {
+  return <div>页面不存在，请检查地址</div>;
 }
 
 export default App;
