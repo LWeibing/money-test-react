3e10af31c0029621520aea3448d14df534f3e0ab
Clown <1104480806@qq.com>
2020-08-09 23:10:02 +0800

    完成 Type 样式

--- src/components/Money/TypeSection.tsx
+++ src/components/Money/TypeSection.tsx
@@ -2,14 +2,35 @@ import styled from 'styled-components';
 import React from 'react';
 
 const TypeSection = styled.section`
-  
-`
+  > ol{
+    background: #eee;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+     >li{
+       width: 50%;
+       line-height: 64px;
+       display: flex;
+       justify-content: center;
+       align-items: center;
+       position: relative;
+       &.selected {
+        background: rgb(255, 153, 0);
+        color: white;
+      }
+     }
+  }
+`;
 
-const Type=()=>{
-  return(
+const Type = () => {
+  return (
     <TypeSection>
+      <ol>
+        <li className="selected">支出</li>
+        <li>收入</li>
+      </ol>
     </TypeSection>
-  )
-}
+  );
+};
 
-export default Type
+export default Type;
