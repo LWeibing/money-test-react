1e954361d2e0a3a3d491de8a4ca49efa1ab4dc6e
Clown <1104480806@qq.com>
2020-08-11 17:56:27 +0800

    抽离 useTags

--- src/views/Labels.tsx
+++ src/views/Labels.tsx
@@ -1,10 +1,12 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import {useTags} from '../useTags';
 
 function Labels() {
+  const {tags, setTags} = useTags();
   return (
     <Layout>
-      <h2>标签</h2>
+      {tags.map(tag => <li key={tag}>{tag}</li>)}
     </Layout>
   );
 }
